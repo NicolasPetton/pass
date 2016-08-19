01a7ced2d4223bee318a67f0ef67ee5ceab5c853
Nicolas Petton <nicolas@petton.fr>
2016-08-19 21:37:35 +0200

    Kill all entry buffers when closing the store
    When closing the password store, ask the user if all entry buffers
    should be killed as well.

--- pass.el
+++ pass.el
@@ -115,7 +115,12 @@ Similar to `save-excursion' but only restore the point."
 (defun pass-quit ()
   "Kill the buffer quitting the window."
   (interactive)
-  (quit-window t))
+  (quit-window t)
+  (when (y-or-n-p "Kill all pass entry buffers? ")
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (when (eq major-mode 'pass-view-mode)
+          (kill-buffer buf))))))
 
 (defun pass-next-entry ()
   "Move point to the next entry found."
